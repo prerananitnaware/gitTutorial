85a6dd929f1b2b110b08199179c393ce274c9a4a
prerananitnaware <bahadureprerana1984@github.com>
2024-04-16 13:12:58 +0530

    added header in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Counter from './components/Counter';
+import { Header } from './Header';
 
 function App() {
   return (
     <div className="App">
      <h1>Counter App</h1>
+     <Header/>
      <Counter/>
     </div>
   );
